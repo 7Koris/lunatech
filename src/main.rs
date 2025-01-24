f94d123614bc7037066f315d54357c7b43b8a4a5
Koris <kamranhaq@protonmail.com>
2025-01-24 14:08:16 -0800

    Monitor device by user selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+extern crate cpal;
+
+use std::thread;
+use clap::{value_parser, ArgAction};
+use colored::*;
+use cpal::{traits::{DeviceTrait, StreamTrait}, SampleRate, StreamConfig};
+
+pub mod prompts;
+
+const DEFAULT_SAMPLE_RATE: u32 = 44100;
+const DEFAULT_BUFFER_SIZE: u32 = 256; 
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::Command::new("lunatech")
+        .version("1.0")
+        .author("Koris")
+        .about("lorem ipsum dolor")
+        .arg(
+            clap::Arg::new("sample_rate")
+                .short('r')
+                .long("sample_rate")
+                .help("Sets the sample rate")
+                .action(ArgAction::Set)
+                .value_parser(value_parser!(u32))
+        )
+        .arg(
+            clap::Arg::new("buffer_size")
+                .short('b')
+                .long("buffer_size")
+                .help("Sets the buffer size")
+                .action(ArgAction::Set)
+                .value_parser(value_parser!(u32))
+        )
+        .get_matches();
+    let sample_rate = matches.get_one::<u32>("sample_rate").unwrap_or(&DEFAULT_SAMPLE_RATE);
+    let buffer_size = matches.get_one::<u32>("buffer_size").unwrap_or(&DEFAULT_BUFFER_SIZE);
+    
+
+    let host = cpal::default_host();
+    let device = prompts::select_device_by_prompt(&host);
+    monitor_device(&device, *sample_rate, *buffer_size);
+}
+
+fn monitor_device(device: &cpal::Device, sample_rate: u32, buffer_size: u32) {
+    print!("Monitoring device: ");
+    println!("{}", device.name().unwrap().green().bold());
+
+    let config = StreamConfig {
+        channels: 1_u16,// device.default_output_config().unwrap().channels(), TODO: ???
+        sample_rate: SampleRate(sample_rate), 
+        buffer_size: cpal::BufferSize::Fixed(buffer_size),
+    };
+
+    let mut last_time = std::time::Instant::now();
+    let stream = device.build_input_stream(
+        &config,
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {   
+            let latency = last_time.elapsed().as_secs_f32();
+            last_time = std::time::Instant::now();
+            let framerate = 1_f32 / latency;
+            println!("{:?} frames/sec", framerate.round());
+        },
+        move |_| {
+            // TODO: Error handling
+        },
+        None
+    ).unwrap();
+
+    stream.play().unwrap();
+    thread::park();
 }
---
+++ src/prompts.rs
@@ -0,0 +1,49 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+use colored::Colorize;
+use gag::Gag;
+
+fn prompt_user_input(patterns: &[String], prompt: String) -> String {
+    loop {
+        println!("{}", prompt); 
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        if patterns.contains(&input.to_string()) {
+            return input.to_string();
+        }
+        println!("{}", "invalid input".red().bold());
+    }
+}
+
+pub fn select_device_by_prompt(host: &cpal::Host) -> cpal::Device{
+    let _print_gag = Gag::stderr().unwrap();
+
+    println!("Select input or output device");
+    let prompt = format!("Enter {} for input device, {} for output device", "1".green().bold(), "2".green().bold());
+    let input_device = prompt_user_input(&["1".to_string(), "2".to_string()], prompt);
+    println!();
+    
+    let devices: Vec<cpal::Device> = if input_device == "1" {
+        println!("Select an input device");
+        host.input_devices()
+        .unwrap()
+        .collect::<Vec<cpal::Device>>()
+    } else {
+        println!("Select an output device");
+        host.output_devices()
+        .unwrap()
+        .collect::<Vec<cpal::Device>>()
+    };
+    
+    let device_indices = (0..devices.len()).map(|x| x.to_string()).collect::<Vec<String>>();    
+    for (i, x) in devices.iter().enumerate() {
+        println!("{} {}", i.to_string().green().bold(), x.name().unwrap());
+    }
+
+    println!();
+    let prompt = format!("Enter a number between {} and {}", "0".green().bold(), (devices.len() - 1).to_string().green().bold());
+    let device_index = prompt_user_input(&device_indices, prompt);
+    println!();
+    
+    devices[device_index.parse::<usize>().unwrap()].clone()
+}
