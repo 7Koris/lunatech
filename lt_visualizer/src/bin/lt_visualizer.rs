9e1e018aeb0bf21d8c643a02b3efa66e8e284270
Koris <kamranhaq@protonmail.com>
2025-02-10 14:01:14 -0800

    Bevy app skeleton

--- lt_visualizer/src/bin/lt_visualizer.rs
+++ lt_visualizer/src/bin/lt_visualizer.rs
@@ -1,8 +1,18 @@
 use lt_client::client::LunaTechClient;
+use bevy::prelude::*;
 
 fn main() {
     let client = LunaTechClient::new(3000);
-    loop {
-        client.get_osc_features();
-     }
+    let mut app = App::new();
+    app.add_plugins((
+        DefaultPlugins,
+    ))
+    .add_systems(Startup, setup);
+    app.run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2d);
 }
